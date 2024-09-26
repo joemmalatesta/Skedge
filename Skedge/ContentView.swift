a30bf6ffb12620b01ad68d93123bd584c84f3821
Joe <joemmalatesta@gmail.com>
2024-09-26 15:48:54 -0400

    Initial Commit

---
+++ Skedge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Skedge
+//
+//  Created by Joe Malatesta on 9/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skedge/SkedgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkedgeApp.swift
+//  Skedge
+//
+//  Created by Joe Malatesta on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SkedgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
